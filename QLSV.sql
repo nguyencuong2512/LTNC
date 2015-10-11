aea7517617d3cadca4b85f72866f99b92d64e6ef
=nguyen cuong <=nguyencuong79779@gmail.com>
2015-10-11 11:02:25 +0700

    Thay doi sql

--- QLSV.sql
+++ QLSV.sql
@@ -26,13 +26,9 @@ select * from SINHVIEN
 
 insert into SINHVIEN values('Nguyen Chi Thien','10-24-1992',1,'19 NHT',09444444,'CNTT')
 insert into student values('Dan Truong','11-04-1991',1,'19 NHT',09444444,'CNTT')
-insert into student values('Cam Ly','07-13-1990',0,'19 NHT',09444444,'CNTT')
-insert into student values('Pham Bang Bang','02-11-1993',0,'19 NHT',09444444,'CNTT')
 
 select *from MONHOC
 insert into MONHOC values('M1','LTNC',30)
-insert into MONHOC values('M2','HDT',30)
+
 select *from KHOA
 insert into KHOA values('CNTT','KHOA CONG NGHE THONG TIN')
-insert into KHOA values('TOAN','KHOA TOAN')
-
